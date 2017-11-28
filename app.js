7f543e32ed8293c5bec05a42bc8eeb2a944a7b64
Crystalhills <timi@crystalhills.org>
2017-11-28 17:52:33 +0100

    intial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var index = require('./routes/index');
+var categories = require('./routes/categories');
+var customers = require('./routes/customers');
+var products = require('./routes/products');
+
+// //Connect to Mongo DB
+mongoose.connect('mongodb://localhost/invoice');
+var db = mongoose.connection;
+
+
+var app = express();
+
+
+//Body Paser Mw
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', index);
+// app.use('/api', categories);
+// app.use('/api', customers);
+app.use('/api', products);
+
+
+var port = 3000;
+app.listen(port, function() {
+    console.log("Server Started On Port" + port);
+})
---
+++ routes/products.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+product = require('../models/products');
+
+
+// API GET REQUEST FOR ALL productS
+router.get('/products', function(req, res, next) {
+    product.getProducts(function(err, product) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(product);
+    });
+});
+
+//  API GET REQUEST FOR ONE productS
+router.get('/products/:_id', function(req, res) {
+    product.getProductsById(req.params._id, function(err, product) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(product);
+    });
+});
+
+// API POST REQUEST
+router.post('/products', function(req, res, next) {
+    var products = req.body;
+    product.addProducts(products, function(err, products) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(product);
+    });
+});
+
+// API PUT REQUEST
+router.put('/products/:_id', function(req, res, next) {
+    var id = req.params._id
+    var products = req.body;
+    product.updateProducts(id, products, {}, function(err, products) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(products);
+    });
+});
+
+//  API Delete REQUEST FOR ONE productS
+router.delete('/products/:_id', function(req, res) {
+    var id = req.params._id
+    product.deleteProducts(id, function(err, product) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(product);
+    });
+});
+
+module.exports = router;
