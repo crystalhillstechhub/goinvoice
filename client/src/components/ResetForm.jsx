66040aa1e39b1edffbbf7e031a950ca25fda4b47
Ajilore Raphael Olamide <raphealolams@gmail.com>
2017-12-12 17:12:06 +0100

    new changes

---
+++ client/src/components/ResetForm.jsx
@@ -0,0 +1,50 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { Card, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+
+const ResetForm = ({
+  onSubmit,
+  onChange,
+  errors,
+  successMessage,
+  user
+}) => (
+  <Card className="container">
+    <form action="/" onSubmit={onSubmit}>
+      <h2 className="card-heading">Login</h2>
+
+      {successMessage && <p className="success-message">{successMessage}</p>}
+      {errors.summary && <p className="error-message">{errors.summary}</p>}
+
+      <div className="field-line">
+        <TextField
+          floatingLabelText="Email"
+          name="email"
+          errorText={errors.email}
+          onChange={onChange}
+          value={user.email}
+        />
+      </div>
+
+      <div className="button-line"> 
+        <RaisedButton type="submit" label="Log in" primary />
+      </div>
+      <CardText>Back <Link to={'/login'}>Login</Link>.</CardText>
+      
+      
+    </form>
+  </Card>
+);
+
+ResetForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+  successMessage: PropTypes.string.isRequired,
+  user: PropTypes.object.isRequired
+};
+
+export default ResetForm;
