12e16049690103e0f1e1b871d0ab75711f71ae82
Ajilore Raphael Olamide <raphealolams@gmail.com>
2017-12-14 19:50:36 +0100

    new

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ const router = new express.Router();
  *
  * @param {object} payload - the HTTP body message
  * @returns {object} The result of validation. Object contains a boolean validation result,
- *                   errors tips, and a global message for the whole form.
+ *errors tips, and a global message for the whole form.
  */
 function validateSignupForm(payload) {
   const errors = {};
