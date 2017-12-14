b87281ae33f60798789de787d7cc9a7deb99e0f8
akintola olalekan <akinlekan@gmail.com>
2017-12-14 12:01:10 +0100

    Added form states definition

--- src/components/Products.js
+++ src/components/Products.js
@@ -30,19 +30,20 @@ class Products extends Component {
   }
 
   render() {
+    const { productName, productDescription, productExpiry } = this.state;
     return (
       <div>
         <Header />
         <div className="row">
           <form className="col s6" onSubmit={this.onSubmit}>
               <div className="input-field">
-               Product Name: <input id="first_name" type="text" className="validate" name="" />
+              Product Name: <input id="first_name" type="text" className="validate" onChange={this.onChange} name="productName" value={productName} />
               </div>
               <div className="input-field">
-             Product Description: <textarea className="materialize-textarea validate" name=""></textarea>
+              Product Description: <textarea className="materialize-textarea validate" onChange={this.onChange} name="productDescription" value={productDescription}></textarea>
               </div>
               <div className="input-field">
-               Product Expiry Date: <input id="password" type="text" className="validate" name="" />
+              Product Expiry Date: <input id="password" type="text" className="validate" onChange={this.onChange} name="productExpiry" value={productExpiry}/>
               </div>
             <input type="submit" value="Add Product" className="btn btn-success" />
           </form>
